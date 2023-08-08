2351f2c62a584ca4bc9676d9b0023c55306a824f
leijingzhan <ljz@email.com>
2023-08-08 19:59:22 +0800

    dev-第三次提交

--- app/src/main/java/com/example/wanandroid/MainActivity.kt
+++ app/src/main/java/com/example/wanandroid/MainActivity.kt
@@ -4,8 +4,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    private var a = 0
+    private var b = 0
+    private var c = 0
+    private var d = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }
